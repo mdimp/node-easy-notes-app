59166f184e408c1649a927fabaf029f19b9661a3
dimp <dinakar@outlook.in>
2018-12-30 22:19:07 +0530

    public ip

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get('/', (req, res) => {
 require('./app/routes/note.routes.js')(app);
 
 // listen for requests
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
     console.log("Server is listening on port 3000");
 });
